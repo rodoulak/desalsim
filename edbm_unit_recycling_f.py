78b008e4e54437b21ddeff4ff1b740dd850125a3
Rodoula Ktori <rodoulaktori@tudelft.nl>
2024-02-28 11:24:59 +0100

    Update edbm_unit_recycling_f.py

--- edbm_unit_recycling_f.py
+++ edbm_unit_recycling_f.py
@@ -89,8 +89,8 @@ class EDBMCalc:
         self.CH_s_in=CH/MW_H
         #Create lists for initial concentration in each channel 
         self.Ci_s_in=[self.CNa_s_in,self.CCl_s_in,self.CK_s_in,self.CMg_s_in,self.CCa_s_in,self.CSO4_s_in,self.CHCO3_s_in, self.CH_s_in,self.COH_s_in]
-        self.Ci_b_in=[Cb1/MW_Na, Cb2/ MW_Cl,Cb3/MW_K,Cb4/MW_Mg,Cb5/MW_Ca,Cb6/MW_SO4,Cb7/MW_HCO3,1e-7,1e-7]
-        self.Ci_a_in=[Ca1/MW_Na,Ca2/MW_Cl,Ca3/MW_K,Ca4/MW_Mg,Ca5/MW_Ca,Ca6/MW_SO4,Ca7/MW_HCO3,1e-7,1e-7]
+        self.Ci_b_in=[Cb1/MW_Na, Cb2/ MW_Cl,Cb3/MW_K,Cb4/MW_Mg,Cb5/MW_Ca,Cb6/MW_SO4,Cb7/MW_HCO3,CH/MW_H,COH/MW_OH]
+        self.Ci_a_in=[Ca1/MW_Na,Ca2/MW_Cl,Ca3/MW_K,Ca4/MW_Mg,Ca5/MW_Ca,Ca6/MW_SO4,Ca7/MW_HCO3,CH/MW_H,COH/MW_OH]
         
         #save initial concentration before recirculation: 
         self.Ci_s_in_0=self.Ci_s_in
@@ -229,13 +229,14 @@ class EDBMCalc:
         
         #Calculation of voltage needed and electromotive force 
         self.c1_emf=math.log(((self.Ci_a_in[7]+self.Ci_a_out[7])/2)/Cm_bp_H)        
-        self.c2_emf=math.log(Cm_bp_OH/((self.Ci_b_in[8]+self.Ci_b_out[8])/2))        
-        self.c3_emf=math.log(((self.Ci_s_in[1]+self.Ci_s_out[1])/2)/((self.Ci_a_in[1]+self.Ci_a_out[1])/2))
+        self.c2_emf=-math.log(Cm_bp_OH/((self.Ci_b_in[8]+self.Ci_b_out[8])/2))        
+        self.c3_emf=-math.log(((self.Ci_s_in[1]+self.Ci_s_out[1])/2)/((self.Ci_a_in[1]+self.Ci_a_out[1])/2))
         self.c4_emf=math.log(((self.Ci_b_in[0]+self.Ci_b_out[0])/2)/((self.Ci_s_in[0]+self.Ci_s_out[0])/2))  
         
             #EMF: electromotive force (V) 
         self.EMF=((R_const*T/z/F)*(self.c1_emf+self.c2_emf+self.c3_emf+self.c4_emf))*self.N_trip   
-        
+        print("EMF IS "+str(self.EMF))
+    
             #V_ext: Voltage needed (V) 
         self.V_ext=self.EMF+((self.I_ext*R_int)/(A*10000))*self.N_trip
         
@@ -268,31 +269,48 @@ class EDBMCalc:
         
         #Calculate recycling, outlet and new feed flow rate in each channel 
             #Salt channel 
+        #Calculate recycling flow rate in salt stream 
         self.Q_s_r=rs*self.M_s_out_t/self.d_s_out
-        self.Q_s_t_out=self.Q1_s_in/(1-rs)
+        #Calculate total outlet (including recycling stream) from salt stream 
+        self.Q_s_t_out=self.Q1_s_in*(1+rs)
+        #Calculate final outlet flowrate in salt stream 
         self.Q_s_out=self.Q1_s_in
+        #Calculate total inflow rate (including recycling)
         self.Q_s_in_t=self.Q1_s_in+self.Q_s_r
+        #Calculate the mass flowrates 
         self.M_s_r=self.Q_s_r/self.d_s_out
         self.M_s_t_out=self.Q_s_t_out/self.d_s_out
         self.M_s_in_t=self.Q1_s_in/d_in+self.Q_s_r/self.d_s_out
 
+
             #Acid channel 
+        #Calculate recycling flow rate in acid stream 
         self.Q_a_r=r*self.M_a_out_t/self.d_a_out
-        self.Q_a_t_out=self.Q_a_r/r
+        #Calculate total outlet (including recycling stream) from acid stream
+        self.Q_a_t_out=self.Q_a_r*(1+r)
+        #Calculate final outlet flowrate in acid stream 
         self.Q_a_out=self.Q1_a_in
+        #Calculate total inflow rate (including recycling) to acid stream 
         self.Q_a_in_t=self.Q1_a_in+self.Q_a_r
+        #Calculate the mass flowrates 
         self.M_a_r=self.Q_a_r/self.d_a_out
         self.M_a_t_out=self.Q_a_t_out/self.d_a_out
         self.M_a_in_t=self.Q1_a_in/d_in+self.Q_a_r/self.d_a_out
 
+
             #Base channel 
+        #Calculate recycling flow rate in base stream 
         self.Q_b_r=r*self.M_b_out_t/self.d_b_out
-        self.Q_b_t_out=self.Q1_b_in/(1-r)
+        #Calculate total outlet (including recycling stream) from base stream
+        self.Q_b_t_out=self.Q1_b_in*(1+r)
+        #Calculate final outlet flowrate in base stream 
         self.Q_b_out=self.Q1_b_in
+        #Calculate total inflow rate (including recycling) to base stream 
         self.Q_b_in_t=self.Q1_b_in+self.Q_b_r
+        #Calculate the mass flowrates 
         self.M_b_r=self.Q_b_r/self.d_b_out
         self.M_b_t_out=self.Q_b_t_out/self.d_b_out
-        self.M_b_in_t=self.Q1_b_in/d_in+self.Q_b_r/self.d_b_out       
+        self.M_b_in_t=self.Q1_b_in/d_in+self.Q_b_r/self.d_b_out      
 
         #Calculation of new concentration 
         for i in range(len(self.Ci_a_in)):
@@ -342,9 +360,7 @@ R_const=8.314462618 #kg⋅m2⋅s−2⋅K−1⋅mol−1
 R_int=45 #ohm cm2
 z=1
 npump=0.8 #pump efficiency (units: -)
-dp_f=0.1 #pressure drop for feed (units: bar)
-dp_r=1.2 #pressure drop for recycling (units: bar)
-dp_p=1 #pressure drop for extracting products (units: bar)
+dp=1 #pressure drop (units: bar)
 
 #Membrane characteristics
 
@@ -387,16 +403,10 @@ Q_in_edbm=1000
 Mw_in=Q_in_edbm/d_in 
 
 #Set number of triplets 
-N_trip=50
+N_trip=50 #range: 20-200 triplets based on the inlet flow rate
 
-#Set membrane area based on the feed flow rate 
-A=0.4#scaleup.scaleup(19.2, 300, Q_in_edbm)/3 
-
-"or"
-# N_trip=round(scaleup.scaleup(20, 300, Q_in_edbm),0)       
-# A=0.4#19/40
-# A_tot=19*Q_in_edbm/60 #scaleup.scaleup(0.19, 100, Q_in_edbm)#19*Q_in_edbm/60/3#
-# N_trip=round(A_tot/A,0)/10
+#Set membrane area based on the feed flow rate, m2 
+A=0.4 #range: 0.1-1
 
 #Initialize concentration of Na in salt channel 
 Cna_s=[]
@@ -413,7 +423,7 @@ edbm_dat.salt_channel()
 Cna_s.append(edbm_dat.Ci_s_out[0])
 
 #Loop for the recycling process 
-while (edbm_dat.Ci_b_out[0]<1) and (edbm_dat.Ci_s_out[0]>=0.05):
+while (edbm_dat.Ci_b_out[0]<1) and (edbm_dat.Ci_s_out[0]>=0.5):
     edbm_dat.recycl_below1M()
     edbm_dat.acid_channel()
     edbm_dat.base_channel()
@@ -426,7 +436,7 @@ while (edbm_dat.Ci_b_out[0]<1) and (edbm_dat.Ci_s_out[0]>=0.05):
 # Loop for recycling at higher concentration
 for i in range(1): 
     edbm_dat.recycl_1M()
-    while (edbm_dat.Ci_b_out[0]<1) and (edbm_dat.Ci_s_out[0]>=0.05):
+    while (edbm_dat.Ci_b_out[0]<1) and (edbm_dat.Ci_s_out[0]>=0.5):
         edbm_dat.recycl_below1M()
         edbm_dat.acid_channel()
         edbm_dat.base_channel()
@@ -463,7 +473,7 @@ Cb_out_g=[Cb_out[0]*MW_Na, Cb_out[1]*MW_Cl, Cb_out[2]*MW_K, Cb_out[3]*MW_Mg, Cb_
 M_b_out=edbm_dat.M_b_out_f*N_trip
 print("M_b_out is "+str(M_b_out)+"kg/hr")
 M_b_out_r=edbm_dat.M_b_r*N_trip
-
+print("M_b_out recycling is "+str(M_b_out_r)+"kg/hr")
     #Volumetric flow rate 
 Q_b_out=edbm_dat.Q_b_out*N_trip
 print("Q_b_out is "+str(Q_b_out)+"l/hr")
@@ -490,7 +500,6 @@ print("Q_a_out is "+str(Q_a_out)+"l/hr")
     #Conversion to solid 
 M_HCl_out=Q_a_out*constants.MW_HCl/1000 #kg/hr
 
-
 #Calculate required amount of water for the operation mode 
 Q_w_in=2*Q_in_edbm
 
@@ -499,19 +508,18 @@ bal=(Q_in_edbm*d_in+2*Q_in_edbm)-M_a_out-M_s_out-M_b_out
 print("balance edbm is "+str(bal))
 
 #Energy consumption 
-
+V_ext=edbm_dat.V_ext #xternal 
 #Calculate energy consumption for pumping 
-Ppump=(3*Q_in_edbm*dp_f+Q_s_out*dp_p+Q_b_out*dp_p+Q_a_out*dp_p+edbm_dat.Q_s_r*N_trip*dp_r+edbm_dat.Q_a_r*N_trip*dp_r+edbm_dat.Q_b_r*N_trip*dp_r)/1000/3600*1e5/npump #units: W -> l to m3 so /1000; bar to J 1e5N/m2*1J/m ; hr to 3660s  
+Ppump=(edbm_dat.Q_s_r*N_trip*dp+edbm_dat.Q_a_r*N_trip*dp+edbm_dat.Q_b_r*N_trip*dp)/1000/3600*1e5/npump #units: W -> l to m3 so /1000; bar to J 1e5N/m2*1J/m ; hr to 3660s  
+
+#Calculate current efficiency 
+Cb_in=[0]
+CE=(Q_b_out)*(Cb_out[0]-Cb_in[0])*F/(3600*N_trip*I_d*A)*100 #%
+print("current efficiency is "+str(CE))
 
 #Total energy consumption 
-E_el_Edbm=48*I_d*A/1000+Ppump/1000
-SEC=(48*I_d*A)/(Q_b_out*(edbm_dat.Ci_b_out[0]-edbm_dat.Ci_b_in[0])*constants.MW_NaOH)
+E_el_Edbm=V_ext*I_d*A/1000+Ppump/1000
+SEC=(V_ext*I_d*A)/(Q_b_out*(edbm_dat.Ci_b_out[0]-edbm_dat.Ci_b_in[0])*constants.MW_NaOH)
 
 print("Total electrical consumption for EDBM is " + str(E_el_Edbm)+ " KW")
 print("sec is "+str(SEC)+"kwh/kg naoh")
-
-#%% sec based on calogero paper
-#SEC= U*Id*A/(60*Qp*(Cp,out-Cp,in)*Mp) where -> U: electrical potential, Id: current density, Cp: product concentration, Mp: molar mass of desired product (g/mol)
-SEC2=(48*I_d*A)/(Q_b_out*(edbm_dat.Ci_b_out[0]-edbm_dat.Ci_b_in[0])*constants.MW_NaOH)
-print("sec2 is " +str(SEC2)) 
-        
