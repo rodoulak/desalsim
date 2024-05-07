2ae969389597c0565d9d5061563c72598bef9eab
rodoulak <r.ktori@tudelft.nl>
2024-05-07 11:18:55 +0200

    updates

--- setup.py
+++ setup.py
@@ -2,12 +2,21 @@
 
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as f: 
+    description =f.read()
+
 setup(
       name='desalination_and_brine_treatment',
-      version='0.1',
+      version='0.2',
       packages=find_packages(),
+      url="https://github.com/rodoulak/Desalination-and-Brine-Treatment-Simulation-",
+      author="rodoulak",
+      author_email="r.ktori@tudelft.nl",
+      license="MIT",
       install_requires=[
           "numpy>=1.0",
           "pandas>=1.0",  # Include pandas as a dependency
-        ],
+      ],
+      long_description= description,
+      long_description_content_type="text/markdown",
       )
